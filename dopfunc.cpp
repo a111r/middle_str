4807a2a88e64dfdfd84fbdb25baeab2a552cf114
a111r <92301153+a111r@users.noreply.github.com>
2021-11-18 10:32:00 +0300

    Update dopfunc.cpp

--- dopfunc.cpp
+++ dopfunc.cpp
@@ -10,37 +10,43 @@ long long itc_len(string str)
     return i;
 }
 
-long long itc_pow(int n, int i)
+long long itc_covert_num(long long num, int ss)
 {
-    if (i == 0)
-        return 1;
-    else
-        return n * itc_pow(n, i - 1);
-}
-
-int itc_stop(string str)
-{
-    int st = int(itc_len(str));
-    int answer = 0;
-    for (int i = 0; i < itc_len(str); i++) {
-        answer += (str[i] - 48) * itc_pow(10, st - 1);
-        st--;
+    long long number = 0, pow = 1;
+    if (num < 0)
+        return (itc_covert_num((num * -1), ss))* -1;
+    while(num > 0){
+        number += num % ss * pow;
+        num /= ss;
+        pow *= 10;
     }
-    return answer;
+    return number;
 }
 
-string convertToBin(string str)
+long long itc_str_to_int(string str)
 {
-    string answer = "";
-    for (int dec = itc_stop(str); dec > 0; dec /= 2)
-        answer = char(dec % 2 + '0') + answer;
-    return answer;
+    long long num, i;
+    num = 0, i = 0;
+    int ch = 0;
+    while(str[i] != '\0'){
+        ch = str[i] - 48;
+        num = num * 10 + ch;
+        i ++;
+    }
+    return num;
 }
 
-string convertToBin(string str)
+string itc_int_to_str(long long num)
 {
-    string answer = "";
-    for (int dec = itc_stop(str); dec > 0; dec /= 2)
-        answer = char(dec % 2 + '0') + answer;
-    return answer;
+    char chislo = '#';
+    string str = "", strin = "";
+    while(num > 0){
+        chislo = (num % 10) + 48;
+        str += chislo;
+        num /=10;
+    }
+    for(long long i = (itc_len(str) - 1); i >= 0; i--){
+        strin += str[i];
+    }
+    return strin;
 }
