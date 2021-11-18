e6a55d0ed6053cfbaaaf81541fd223a06179d142
a111r <92301153+a111r@users.noreply.github.com>
2021-11-18 20:19:10 +0300

    Update func1.cpp

--- func1.cpp
+++ func1.cpp
@@ -52,6 +52,52 @@ bool itc_compare(string s1, string s2)
 }
 
 int itc_countWords(string str)
+{
+    int kol;
+    long long i;
+    bool bol = true;
+    kol = 0, i = 0;
+    while(str[i] != '\0')
+    {
+        if ((str[i] < 65 || (str[i] > 90 && str[i] < 97) || str[i] > 122) && str[i] != 32)
+        {
+            bol = false;
+        }
+        if(str[i] == 32 && bol == true)
+        {
+            kol += 1;
+        }
+        if(str[i] == 32 && bol == false)
+        {
+            bol = true;
+        }
+        i++;
+    }
+    if(bol == false)
+        return kol;
+    kol++;
+    return kol;
+}
+
+bool itc_compare(string s1, string s2)
+{
+    long long len1, len2;
+    len1 = itc_len(s1);
+    len2 = itc_len(s2);
+    long long i, kol;
+    i = 0, kol = 0;
+    if (len1 != len2)
+        return false;
+    while (s1[i] <= '\0')
+    {
+        if (s1[i] != s2[i])
+            return false;
+        i++;
+    }
+    return true;
+}
+
+int itc_countWords(string str)
 {  
     int kol;
     long long i;
