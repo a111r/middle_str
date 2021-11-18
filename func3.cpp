a258d2411a6e6a295547efd4426552e740d72446
a111r <92301153+a111r@users.noreply.github.com>
2021-11-18 20:17:27 +0300

    Update func3.cpp

--- func3.cpp
+++ func3.cpp
@@ -1,1 +1,44 @@
+#include "middle_str.h"
+
+bool itc_isIp(string str)
+{
+    int kol, number;
+    kol = 0,number = 1;
+    if (str == "")
+        return "error";
+    for (long long i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] >= '0' && str[i] <= '9')
+            number = 10 * number + str[i] - 48;
+        else
+        {
+            if (str[i] != 46)
+                return false;
+            if (number == 0)
+                return false;
+            if ((number % 1000) > 255 || (number % 1000) < 0)
+                return false;
+            kol++;
+            number = 1;
+        }
+    }
+    if (number == 0)
+        return false;
+    if ((number % 1000) > 255 || (number % 1000) < 0)
+        return false;
+    kol++;
+    if (kol != 4)
+        return false;
+    return true;
+}
+
+string itc_DecToBin(string str)
+{
+
+}
+
+string itc_decToBase(int num, int base)
+{
+
+}
 
