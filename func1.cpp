c32af87240701f3ba425a7858e8ccad56686f39e
a111r <92301153+a111r@users.noreply.github.com>
2021-11-18 10:03:50 +0300

    Update func1.cpp

--- func1.cpp
+++ func1.cpp
@@ -35,36 +35,20 @@ unsigned char itc_changeCase(unsigned char c)
 
 bool itc_compare(string s1, string s2)
 {
-    for (long long i = 0; i < itc_len(s1); i++)
-    {
-        if (s1[i] != s2[i])
-        {
-            return false;
-            break;
-        }
-
-    }
+    if (itc_len(s1) != itc_len(s2))
+        return false;
+    for (long long i = 0; i != itc_len(s1) - 1; i++) if (s1[i] != s2[i])
+        return false;
     return true;
 }
 
 int itc_countWords(string str)
 {
-    long long counter = 0;
-    for(long long i = 1; str[i] < '\0';  i++)
+    int answer = 0;
+    str += ' ';
+    for (int i = 0; str[i + 1] != '\0'; i++)
     {
-        if (str[i] == ' ' && (str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z'))
-        {
-            counter += 1;
-        }
-
+        if (itc_isLetter(str[i]) && !itc_isLetter(str[i + 1])) answer++;
     }
-    if (str[itc_len(str) - 2] == ' ')
-    {
-        counter -= 1;
-    }
-    if (str[0] == ' ')
-    {
-        counter -= 1;
-    }
-    return counter;
+    return answer;
 }
