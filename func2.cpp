cc4a10a48ad63388c7db3cb088632bd6d17f72de
a111r <92301153+a111r@users.noreply.github.com>
2021-11-18 19:38:50 +0300

    Update func2.cpp

--- func2.cpp
+++ func2.cpp
@@ -2,40 +2,37 @@
 
 string itc_maxCharWord(string str)
 {
-    long long max, length;
-    max = -1;
-    length = 0;
-    string word = "";
-    string answer = "";
-    if (str == "")
+	if (itc_countWords(str) < 2)
         return "error";
-    for (long long i = 0; str[i] != '\0'; i++)
-    {
-        if ((65 <= str[i] && str[i] <= 90) || (97 <= str[i] && str[i] <= 122))
-        {
-            word += str[i];
-            length ++;
-        }
-        else
-        {
-            if (max < length)
-                answer = word;
-            length = 0;
-            word = "";
-        }
-    }
-    if (max < length)
-        answer = word;
-    return answer;
+	str = itc_rmFreeSpace(str);
+	string word = "";
+	string answer = "";
+	for (int i = 0, n = 0; str[i] != '\0'; i++)
+	{
+		if((str[i] != ' ' || str[i+1] == '\0') && !(str[i] == ',' || str[i] == '.' || str[i] == '!' || str[i] == '?'))
+            word += str[i], n++;
+		if ((str[i] == ' ' || str[i + 1] == '\0') && isValidWord(word))
+		{
+			if (itc_len(word) > itc_len(answer))
+			{
+			answer = word;
+			word = "";
+			n = 0;
+            }
+		}
+	}
+	if (answer == "")
+        answer = "error";
+	return answer;
 }
 
 char itc_sameChar(string str)
 {
-    for ( long long i = 0; str[i] != '\0'; i++)
+    for (long long i = 0; str[i] != '\0'; i++)
         {
         if (( str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z'))
         {
-            for ( long long j = 0; str[j] != '\0'; j++)
+            for (long long j = 0; str[j] != '\0'; j++)
             {
                 if ( str[j] == str[i])
                 {
@@ -49,92 +46,46 @@ char itc_sameChar(string str)
 
 bool itc_isFirstInSecond(string s1, string s2)
 {
-    bool state;
-    state = false;
-    long long length ;
-    length = 0;
-    for (long long i = 0; s2[i] != '\0'; i++)
+    if(s1 == "" or s2 == "")
+        return false;
+    for(int i = 0; itc_len(itc_slice_str(s2, i, i + itc_len(s1) - 1)) == itc_len(s1); i++)
     {
-        if (s1[length] == s2[i])
-        {
-            length ++;
-        }
-        else
-        {
-            state = (length == itc_len(s1));
-            length = 0;
-        }
+        if(itc_slice_str(s2, i, i + itc_len(s1) - 1) == s1)
+            return true;
+
     }
-    state = (length == itc_len(s1));
-    return state;
+    return false;
 }
+
 
 string itc_Cezar(string str, int k)
 {
-    int number;
-    char chislo;
-    string res;
-    for ( long long i = 0; str[i] != '\0'; i++){
-        number = str[i];
-        if( number > 65 && number < 90)
-        {
-            if (number +  k > 90)
-            {
-                chislo = 64 + (number + k - 90);
-            }
-            else if ( chislo + k < 65)
-            {
-                chislo = 91 - (65 - number - k );
-            }
-            else
-            {
-            chislo = number +  k;
-            }
-        }
-        else if(chislo > 97 && chislo < 122)
-        {
-            if ( number +  k  > 122)
-            {
-                chislo = 96 + (number + k - 122);
-            }
-            else if(number + k < 97)
-            {
-                chislo = 122 - (97 - number- k );
-            }
-            else
-            {
-                chislo = number + k;
-            }
-        }
-        else
-        {
-            return "-1";
-        }
-        res += chislo;
-    }
-    return res;
-    return "-1";
+	k = (k % 26 + 26) % 26;
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = (str[i] - 'a' + k) % 26 + 'a';
+		else if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] = (str[i] - 'A' + k) % 26 + 'A';
+	}
+	return str;
 }
 
 string itc_rmFreeSpace(string str)
 {
-    string word, slovo;
-    word = "";
-    slovo = "";
-    for (long long i = 0; str[i] != '\0'; i++)
+    long long i;
+    i = 0;
+    string str1 = "";
+    while(str[i] != '\0')
     {
-        if (str[i] == ' ')
-        {
-            slovo += str[i];
-        }
-        else
-        {
-            if (slovo != "")
-                slovo = " ";
-            word += slovo;
-            slovo = "";
-            word += str[i];
-        }
+        if((str[i] == 32 && str[i - 1] != 32) || str[i] < 32 || str[i] > 32)
+            str1 += str[i];
+        i++;
     }
-    return word;
+    if (str1[0] == 32)
+    str1 = itc_slice_str(str1, 1 , itc_len(str1) - 1);
+    if((str1[itc_len(str1) - 1]) == 32)
+        str1 = itc_slice_str(str1, 0 , itc_len(str1) - 2);
+    return str1;
 }
+
