ad792b9e529a414f7b4b828761032a46437bdd7e
a111r <92301153+a111r@users.noreply.github.com>
2021-11-18 20:21:04 +0300

    Update func2.cpp

--- func2.cpp
+++ func2.cpp
@@ -57,3 +57,34 @@ bool itc_isFirstInSecond(string s1, string s2)
     return false;
 }
 
+
+string itc_Cezar(string str, int k)
+{
+	k = (k % 26 + 26) % 26;
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = (str[i] - 'a' + k) % 26 + 'a';
+		else if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] = (str[i] - 'A' + k) % 26 + 'A';
+	}
+	return str;
+}
+
+string itc_rmFreeSpace(string str)
+{
+    long long i;
+    i = 0;
+    string str1 = "";
+    while(str[i] != '\0')
+    {
+        if((str[i] == 32 && str[i - 1] != 32) || str[i] < 32 || str[i] > 32)
+            str1 += str[i];
+        i++;
+    }
+    if (str1[0] == 32)
+    str1 = itc_slice_str(str1, 1 , itc_len(str1) - 1);
+    if((str1[itc_len(str1) - 1]) == 32)
+        str1 = itc_slice_str(str1, 0 , itc_len(str1) - 2);
+    return str1;
+}
