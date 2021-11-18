0def645b65b7a8e74fa04c7bc80ae97aaacb385a
a111r <92301153+a111r@users.noreply.github.com>
2021-11-18 21:33:04 +0300

    Update dopfunc.cpp

--- dopfunc.cpp
+++ dopfunc.cpp
@@ -12,32 +12,6 @@ long long itc_len(string str)
     return i;
 }
 
-bool isLetter(char word)
-{
-    return word >= 'a' && word <= 'z' || word >= 'A' && word <= 'Z';
-}
-
-bool isValidWord(string str)
-{
-    bool isValid = true;
-    if(str == "")
-        return false;
-    if (!isLetter(str[0]))
-        return false;
-    int number;
-    number = 1;
-    for (int i = 0; str[i+1]!='\0'; i++, number++)
-    {
-        if (!isLetter(str[i]) && isValid)
-        return false;
-    }
-
-    if(!isLetter(str[number - 1]))
-        return str[number - 1] == '.' || str[number - 1] == ',' || str[number - 1] == '?' || str[number - 1] == '!';
-
-    return true;
-}
-
 string itc_slice_str(string str, int start, int end1)
 {
     string result;
