b6d9b1f4e0c21b79f4cdc6cbcca56fcd457095ff
a111r <92301153+a111r@users.noreply.github.com>
2021-11-18 20:17:41 +0300

    Update dopfunc.cpp

--- dopfunc.cpp
+++ dopfunc.cpp
@@ -1,5 +1,4 @@
 #include "middle_str.h"
-
 long long itc_len(string str)
 {
     long long res = 0;
@@ -10,3 +9,53 @@ long long itc_len(string str)
     return i;
 }
 
+bool isLetter(char word)
+{
+    return word >= 'a' && word <= 'z' || word >= 'A' && word <= 'Z';
+}
+
+bool isValidWord(string str)
+{
+    bool isValid = true;
+    if(str == "")
+        return false;
+    if (!isLetter(str[0]))
+        return false;
+    int number;
+    number = 1;
+    for (int i = 0; str[i+1]!='\0'; i++, number++)
+    {
+        if (!isLetter(str[i]) && isValid)
+        return false;
+    }
+
+    if(!isLetter(str[number - 1]))
+        return str[number - 1] == '.' || str[number - 1] == ',' || str[number - 1] == '?' || str[number - 1] == '!';
+
+    return true;
+}
+
+string itc_slice_str(string str, int start, int end)
+{
+    string result;
+    result = "";
+    if ( end >= itc_len(str))
+        end = itc_len(str) - 1;
+    if(start > end)
+        return str;
+    else if(start == (itc_len(str) - 1))
+    {
+        result += str[itc_len(str) - 1];
+        return result;
+    }
+    else
+    {
+        for ( long long i = start; i <= end; i++)
+        {
+        result += str[i];
+        }
+    return result;
+    }
+
+    return "-1";
+}
