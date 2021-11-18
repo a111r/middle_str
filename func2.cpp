d3fa0d0b77e27bf3f51fab2615395ee011c2e8b2
a111r <92301153+a111r@users.noreply.github.com>
2021-11-18 22:33:56 +0300

    Update func2.cpp

--- func2.cpp
+++ func2.cpp
@@ -69,18 +69,11 @@ string itc_Cezar(string str, int k)
 
 string itc_rmFreeSpace(string str)
 {
-    long long i;
-    i = 0;
-    string str1 = "";
-    while(str[i] != '\0')
-    {
-        if((str[i] == 32 && str[i - 1] != 32) || str[i] < 32 || str[i] > 32)
-            str1 += str[i];
-        i++;
-    }
-    if (str1[0] == 32)
-    str1 = itc_slice_str(str1, 1 , itc_len(str1) - 1);
-    if((str1[itc_len(str1) - 1]) == 32)
-        str1 = itc_slice_str(str1, 0 , itc_len(str1) - 2);
-    return str1;
+	string str1 = "";
+	int n;
+	n = 0;
+	for (int i = 0; str[i] != '\0'; i++)
+		if (!(str[i] == ' ' && str[i + 1] == ' ' || str[i] == ' ' && str[i + 1] == '\0' || n == 0 && str[i] == ' ')) 
+			str1 += str[i], n++;
+	return str1;
 }
