a27dc1b9fba15785535fac4a94bb13f88681aadd
a111r <92301153+a111r@users.noreply.github.com>
2021-11-19 22:29:03 +0300

    Update middle_str.h

--- middle_str.h
+++ middle_str.h
@@ -9,7 +9,8 @@ using namespace std;
 long long itc_len(string str);
 string itc_slice_str(string str, int start, int end);
 
-int itc_countWords(string str);
+
+bool itc_isDigit(unsigned char c);
 unsigned char itc_toUpper(unsigned char c);
 unsigned char itc_changeCase(unsigned char c);
 bool itc_compare(string s1, string s2);
