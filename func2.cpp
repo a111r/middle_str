73342de4278a8638902a6227898c7326ec535c28
a111r <92301153+a111r@users.noreply.github.com>
2021-11-19 22:33:46 +0300

    Update func2.cpp

--- func2.cpp
+++ func2.cpp
@@ -2,7 +2,27 @@
 
 string itc_maxCharWord(string str)
 {
-    return 0;
+    int strLen = itc_len(str);
+    int counter = 0;
+    string newWord = "";
+    string res = "";
+    for(int i = 0; i < strLen + 1; i++){
+        if((str[i] == ' ' or str[i] == '\0') && (itc_len(newWord) > itc_len(res))){
+            res = newWord;
+            newWord = "";
+        }
+        if((str[i] >= 'A' and str[i] <= 'Z') || (str[i] >= 'a' and str[i] <= 'z')){
+            newWord += str[i];
+        }
+        if(str[i] == ' ' && str[i + 1] != ' '){
+            newWord = "";
+            counter += 1;
+        }
+    }
+    if(counter == 0){
+        return "error";
+    }
+    return res;
 }
 
 char itc_sameChar(string str)
