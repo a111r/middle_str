f5a3509a46efc46c4e5bdea8f184a933a0afdeaa
a111r <92301153+a111r@users.noreply.github.com>
2021-11-22 21:57:26 +0300

    Update func2.cpp

--- func2.cpp
+++ func2.cpp
@@ -58,7 +58,7 @@ char itc_sameChar(string str)
 
 bool itc_isFirstInSecond(string s1, string s2) 
 {
-    long long checker = itc_find_middle_str(s1, s2);
+    long long checker = itc_find_str(s1, s2);
     if (checker == -1)
         return false;
 return true;
