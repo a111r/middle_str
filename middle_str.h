15be660ec467cd105b7b579fce5fbf4c2a299ad7
a111r <92301153+a111r@users.noreply.github.com>
2021-11-22 21:01:07 +0300

    Update middle_str.h

--- middle_str.h
+++ middle_str.h
@@ -23,7 +23,5 @@ bool itc_isFirstInSecond(string s1, string s2);
 string itc_Cezar(string str, int k);
 string itc_rmFreeSpace(string str);
 bool itc_isIp(string str);
-string itc_DecToBin(string str);
-string itc_decToBase(int num, int base);
 
 #endif // MIDDLE_STR_H_INCLUDED
